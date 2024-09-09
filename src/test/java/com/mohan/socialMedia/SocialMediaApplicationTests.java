3b81e9e7301058227b976e3de83546914ff52581
bichitra <bichitramohansngha@gmail.com>
2024-09-09 14:26:39 +0530

    first commit

---
+++ src/test/java/com/mohan/socialMedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan.socialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
